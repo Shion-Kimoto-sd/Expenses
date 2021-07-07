9f1e66d4a01e26b3afb972882bb57193339428fa
id <id@IN0013465>
2021-07-07 14:39:48 +0900

    create

---
+++ src/main/java/com/example/demo/ExpensesSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensesSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensesSystemApplication.class, args);
+	}
+
+}
